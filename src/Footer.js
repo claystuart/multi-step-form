bae626cef293af4e1e7509f4e6d2000001be597d
Clay Stuart <clay.stuart@gmail.com>
2023-05-15 14:33:55 -0400

    Create Footer.js and Footer.css.

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import "./Footer.css";
+
+function Footer(){
+    return(
+        <footer>
+            <button className="btn btn-back">
+                Go Back
+            </button>
+            <button className="btn btn-forward">
+                Next Step
+            </button>
+        </footer>
+    );
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Header from './Header'
+import Header from './Header';
+import Footer from './Footer'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
+    <Footer />
   </React.StrictMode>
 );
 

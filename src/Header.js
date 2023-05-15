7ce021a82b377c784f11ad74341e2ef565de1769
Clay Stuart <clay.stuart@gmail.com>
2023-05-15 14:16:23 -0400

    Add classNames to header elements.

--- src/Header.js
+++ src/Header.js
@@ -3,18 +3,22 @@ import "./Header.css";
 function Header() {
     return (
         <header>
-            <nav>
-                <input type="radio" id='page1' name="nav" value='1'></input>
-                <label for="page1" className="nav-circle">1</label>
+            <nav className="siteNav">
+                <input className="nav-radio" type="radio" id='page1'
+                    name="nav" value='1'></input>
+                <label for="page1" className="nav-label">1</label>
 
-                <input type="radio" id='page2' name="nav" value='2'></input>
-                <label for="page2" className="nav-circle">2</label>
+                <input className="nav-radio" type="radio" id='page2'
+                    name="nav" value='2'></input>
+                <label for="page2" className="nav-label">2</label>
 
-                <input type="radio" id='page3' name="nav" value='3'></input>
-                <label for="page3" className="nav-circle">3</label>
+                <input className="nav-radio" type="radio" id='page3'
+                    name="nav" value='3'></input>
+                <label for="page3" className="nav-label">3</label>
 
-                <input type="radio" id='page4' name="nav" value='4'></input>
-                <label for="page4" className="nav-circle">4</label>
+                <input className="nav-radio" type="radio" id='page4'
+                    name="nav" value='4'></input>
+                <label for="page4" className="nav-label">4</label>
             </nav>
         </header>
     );

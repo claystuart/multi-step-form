6b0a7668f955a43cc360cb4293d04943f2627df5
Clay Stuart <clay.stuart@gmail.com>
2023-05-18 17:55:43 -0400

    Create and style 404 page.

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,6 +1,19 @@
-const NoPage = () => {
-    return <h1>404</h1>;
-  };
-  
-  export default NoPage;
-  
+export default function NoPage() {
+  let noPageStyle = {
+    width: 343,
+    height: 367,
+    background: "white",
+    borderRadius: 10,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+  return (
+    <div style={noPageStyle}>
+      <h1 style={{fontSize: "60px"}}>404</h1>
+      <h2>No Page Found</h2>
+    </div>
+  )
+};
+

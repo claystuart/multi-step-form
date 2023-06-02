3a82ea016fc78a0477832a24675059fa00073ff7
Clay Stuart <clay.stuart@gmail.com>
2023-06-02 15:51:25 -0400

    Styled ThankYou and removed inFunction styling.

--- src/pages/ThankYou.jsx
+++ src/pages/ThankYou.jsx
@@ -1,34 +1,13 @@
 import thankYouImage from "../images/icon-thank-you.svg"
+import "../css/thankYou.css"
 
 export default function ThankYou() {
-    let thankYouStyle = {
-        width: 343,
-        height: 400,
-        background: "white",
-        borderRadius: 10,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center"
-    }
-
-    let imgStyle = {
-        width: 56,
-        height: 56,
-        marginTop: 79
-    }
-
-    let noteStyle = {
-        width: 295,
-        height: 125,
-        textAlign: "center",
-        lineHeight: "25px"
-    }
 
     return (
-        <div style={thankYouStyle}>
-            <img src={thankYouImage} style={imgStyle} alt="Thank You" />
-            <h1 style={{ marginTop: 24 }}>Thank you!</h1>
-            <h2 style={noteStyle}>Thanks for confirming your subscription! We hope you have fun
+        <div className="thankYouContainer">
+            <img src={thankYouImage} className="imgStyle" alt="Thank You" />
+            <h1 className="thankYou-heading">Thank you!</h1>
+            <h2 className="thankYou-subheading">Thanks for confirming your subscription! We hope you have fun
                 using our platform. If you ever need support, please feel free
                 to email us at support@loremgaming.com.</h2>
         </div>
